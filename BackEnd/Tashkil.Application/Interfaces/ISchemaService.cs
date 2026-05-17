14ebd6620969fd32530baaf211a0c5a9a8fb836d
ABDulRahma-N <matheo5670@gmail.com>
2026-05-17 12:45:52 +0300

    Add SchemaService and ISchemaService interface

---
+++ BackEnd/Tashkil.Application/Interfaces/ISchemaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tashkil.Domain.Entities;
+
+namespace Tashkil.Application.Interfaces
+{
+    public interface ISchemaService
+    {
+            Task<List<Databases>> GetDatabasesAsync();
+            Task<List<Tables>> GetTablesAsync(string databaseName);
+            Task<List<Columns>> GetColumnsAsync(string databaseName, string tableName);
+    }
+}
---
+++ BackEnd/Tashkil.Application/Services/SchemaService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tashkil.Application.Interfaces;
+using Tashkil.Domain.Interfaces;
+
+namespace Tashkil.Application.Services
+{
+    public class SchemaService:ISchemaService
+    {
+        private readonly ISchemaReader _schemaReader;
+        public SchemaService(ISchemaReader schemaReader)
+        {
+            _schemaReader = schemaReader;
+        }
+        public Task<List<Domain.Entities.Databases>> GetDatabasesAsync()
+        {
+            return _schemaReader.GetDatabasesAsync();
+        }
+        public Task<List<Domain.Entities.Tables>> GetTablesAsync(string databaseName)
+        {
+            return _schemaReader.GetTablesAsync(databaseName);
+        }
+        public Task<List<Domain.Entities.Columns>> GetColumnsAsync(string databaseName, string tableName)
+        {
+            return _schemaReader.GetColumnsAsync(databaseName, tableName);
+        }
+    }
+}
