a9600a362d09fc621a1be9a92ce07f125f58b1bf
ABDulRahma-N <matheo5670@gmail.com>
2026-05-17 13:37:27 +0300

    Add NameHelper with Singularize method using Humanizer

---
+++ BackEnd/Tashkil.Application/Helpers/NameHelper.cs
@@ -0,0 +1,18 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tashkil.Application.Helpers
+{
+
+    public static class NameHelper
+    {
+        public static string Singularize(string tableName)
+        {
+            return tableName.Singularize();
+        }
+    }
+}
