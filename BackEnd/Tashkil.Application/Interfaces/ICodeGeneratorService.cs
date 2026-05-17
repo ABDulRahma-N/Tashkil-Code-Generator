b975ed596b9b1bfff78fe74284cfce50bf12873a
ABDulRahma-N <matheo5670@gmail.com>
2026-05-17 13:30:23 +0300

    Add ICodeGeneratorService interface and implementation

---
+++ BackEnd/Tashkil.Application/Interfaces/ICodeGeneratorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tashkil.Domain.Entities;
+
+namespace Tashkil.Application.Interfaces
+{
+    public interface ICodeGeneratorService
+    {
+        Task <string> GenerateEntityAsync(string TableName, List<Columns> columns);
+    }
+}
---
+++ BackEnd/Tashkil.Application/Services/CodeGeneratorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tashkil.Application.Interfaces;
+using Tashkil.Domain.Entities;
+
+namespace Tashkil.Application.Services
+{
+    public class CodeGeneratorService : ICodeGeneratorService
+    {
+        public Task<string> GenerateEntityAsync(string TableName, List<Columns> columns)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
