59e9e5080f1cedbf48b6a279467acaa39499413a
ABDulRahma-N <matheo5670@gmail.com>
2026-06-10 18:05:28 +0300

    Add CodeGeneratorService with entity, repository implementation, and interface generation functions

---
+++ FrontEnd/src/services/CodeGeneratorService.js
@@ -0,0 +1,34 @@
+import api from "@/lib/api";
+
+export async function generateEntity(createEntityDto) {
+  try {
+    const res = await api.post("/CodeGenerator/enerateEntity", createEntityDto);
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching databases:", error);
+    throw error;
+  }
+}
+export async function generateRepositoryImplementation(createRepositoryDto) {
+  try {
+    const res = await api.post(
+      "/CodeGenerator/GenerateRepositoryImplementation",
+      createRepositoryDto,
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching databases:", error);
+    throw error;
+  }
+}
+export async function generateRepositoryInterface(tableName) {
+  try {
+    const res = await api.post("/CodeGenerator/GenerateRepositoryInterface", {
+      TableName: tableName,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching databases:", error);
+    throw error;
+  }
+}
